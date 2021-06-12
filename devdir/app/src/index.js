f0b874a7b65903907606b6d55499488e762a63b3
sirogarasu <sirogarasu.tech@gmail.com>
2021-06-12 11:19:00 +0900

    3目ならべのゲーム勝敗まで＋α（Gameクラスの一部）

--- devdir/app/src/index.js
+++ devdir/app/src/index.js
@@ -21,6 +21,9 @@ import './index.css';
 
     handleClick(i){
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O'
       this.setState({
         squares: squares,
@@ -37,7 +40,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+        status = 'Wineer: ' + winner;
+      } else {
+        status = 'Nest player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -63,6 +72,15 @@ import './index.css';
   }
   
   class Game extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9),fill(null),
+        }],
+        xIsNext: true,
+      }
+    }
     render() {
       return (
         <div className="game">
@@ -76,6 +94,26 @@ import './index.css';
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
