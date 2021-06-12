dcd8b9378dbe1bffa442c70ac1deaa0139162e3c
sirogarasu <sirogarasu.tech@gmail.com>
2021-06-12 11:59:30 +0900

    keyの設定　ここでは手番を使用

--- devdir/app/src/index.js
+++ devdir/app/src/index.js
@@ -74,6 +74,17 @@ import './index.css';
       const current = history[history.length -1 ];
       const winner = calculateWinner(current.squares);
 
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
       let status;
       if(winner){
         status = 'Winner: ' + winner;
@@ -90,7 +101,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
